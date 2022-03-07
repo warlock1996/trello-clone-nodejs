73ace0d0e8827a541dcaf9c9ebc87fa2130170cd
Arslan Ali <arslanali.921996@gmail.com>
2022-03-07 19:03:19 +0500

    add env check for db uris

--- dbconfig.js
+++ dbconfig.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
-let db;
+const mongoose = require('mongoose')
+let db
 const getConnection = async () => {
-        try {
-            if (db) return db
-            db = await mongoose.connect(process.env.MONGO_URI_PROD)
-            console.log("DB connected !")
-            return db
-            
-        } catch (error) {
-            console.error(error.message)
-        }
+	try {
+		if (db) return db
+		const URI = process.env.NODE_ENV === 'development' ? process.env.MONGO_URI_DEV : process.env.MONGO_URI_PROD
+		db = await mongoose.connect(URI)
+		console.log('DB connected !')
+		return db
+	} catch (error) {
+		console.error(error.message)
+	}
 }
 module.exports = getConnection
-
